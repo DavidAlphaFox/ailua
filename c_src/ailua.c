b711339f8d9ec38cb0a639246702d33031dabdeb
David.Gao <david.alpha.fox@gmail.com>
2018-12-06 18:34:23 +0800

    static link with lua

--- c_src/ailua.c
+++ c_src/ailua.c
@@ -6,3 +6,15 @@
 #include <assert.h>
 #include <string.h>
 
+static int
+load(ErlNifEnv* env, void** priv, ERL_NIF_TERM load_info)
+{
+  lua_State *L;
+  L = luaL_newstate(); 
+  return 0;
+}
+static ErlNifFunc nif_funcs[] = {
+};
+
+ERL_NIF_INIT(ailua, nif_funcs, &load, NULL, NULL, NULL);
+
