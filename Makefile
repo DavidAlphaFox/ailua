62d13042fb6cffae513d5f03f983d62a5eecbbcc
David.Gao <david.alpha.fox@gmail.com>
2018-12-06 18:14:56 +0800

    add lua

---
+++ Makefile
@@ -0,0 +1,5 @@
+PROJECT = ailua
+PROJECT_DESCRIPTION = New project
+PROJECT_VERSION = 0.1.0
+
+include erlang.mk
---
+++ c_src/ailua.c
@@ -0,0 +1,8 @@
+#include "erl_nif.h"
+#include "lua.h"
+#include "lauxlib.h"
+#include "lualib.h"
+#include <stdio.h>
+#include <assert.h>
+#include <string.h>
+
