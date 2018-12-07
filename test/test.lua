9a4a603ec92f28e9b2baac887662cd09a0f03d7c
David.Gao <david.alpha.fox@gmail.com>
2018-12-07 12:37:41 +0800

    pass value into lua

--- test/test.lua
+++ test/test.lua
@@ -5,3 +5,13 @@ end
 function hi()
     print("hi there")
 end
+function fib(n)
+    if n < 2 then return 1 end
+    return fib(n - 2) + fib(n - 1)
+  end
+function call_with_id(id)
+    str = "call with id: " .. id 
+    fib = fib(id)
+    str1 = str .. " fib: " .. fib .. "\r\n"
+    print(str1)
+end
